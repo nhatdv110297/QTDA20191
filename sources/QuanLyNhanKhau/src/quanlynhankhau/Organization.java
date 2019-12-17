f90e9a289358ebc33f9ffc4d731575ec691e9ce4
Lân Phùng <vuongbatdansufer@gmail.com>
2019-12-17 10:12:45 +0700

    add new class 6

---
+++ sources/QuanLyNhanKhau/src/quanlynhankhau/Organization.java
@@ -0,0 +1,30 @@
+package entity;
+
+public class Organization extends Entity {
+	
+	private String headquarters;
+	
+	public Organization(String identifier, String name, String describe, String headquarters) {
+		super(identifier, name, describe);
+		this.headquarters = headquarters;
+	}
+
+	public Organization(String identifier, String name, String describe) {
+		super(identifier, name, describe);
+		// TODO Auto-generated constructor stub
+	}
+
+
+	public Organization() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getHeadquarters() {
+		return headquarters;
+	}
+
+	public void setHeadquarters(String headquarters) {
+		this.headquarters = headquarters;
+	}
+
+}
